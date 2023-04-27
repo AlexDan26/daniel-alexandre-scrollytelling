f30f9b441965294933660137a6b36b6ee4d338e6
AlexDan26 <90867270+AlexDan26@users.noreply.github.com>
2023-04-27 10:20:20 -0400

    P2_2_Remise_2

--- script/script.js
+++ script/script.js
@@ -1,24 +1,56 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const bodyScroll = document.querySelector("#sectionSprite");
+const bodyScroll = document.querySelector('.bodySprite');
+
 
 window.addEventListener("scroll", (function animateSprite(){
-    bodyScroll.classList.add(".is-scrolling")
-    console.log("Hello");
-    const minuteur = setTimeout(animateSprite, 200);
-    /*if(minuteur = 0){
-        bodyScroll.classList.remove(".is-scrolling");
-    }*/
+    bodyScroll.classList.add("is-scrolling")
+    const minuteur = setTimeout(() => { bodyScroll.classList.remove("is-scrolling"); }, 1000);
 }
 ) 
 )
-gsap.to('window', {
-    scrollTrigger: {
-      markers: true,
-      trigger: '#sectionSprite',
-    },
-    x: '100%',
-    duration: 2,
-    
-  });
-//gsap.to(window)
+//ANIMATION DU "CALL-TO-ACTION""
+gsap.fromTo("#cta",
+           {y:0},
+           {y:"50px",
+           duration: 1,
+           ease: "none",
+           yoyo:"true",
+           repeat: -1}
+           );
+//ANIMATION DU OVERLAY
+gsap
+  .timeline()
+  .from("#veloD", { y:"100%", ease:"power3" }, "+=1")
+  //.fromTo("#veloD", {x:"0%"},{ x:"5%", ease:"power3", yoyo:"true" }, "+=1")
+  .from("#veloG", { y:"100%", ease:"power3" }, "<=.025")
+  .from("#soleil", { rotation:90, duration:1, transformOrigin:"right -100%", ease:"elastic" })
+  .from("#etoile", { y:"-100%", ease: "elastic"} )
+  .from("#pleineLune", { rotation:90, duration:1, transformOrigin:"right -100%", ease:"elastic" })
+  .from("#lune", { y:"-100%",duration:1, ease: "elastic"} );
+//ANIMATION DES CHAPITRES
+  gsap.timeline()
+  .fromTo("#divin", 
+  { opacity: '0%' }, 
+  { opacity: '100%', duration:2 },"+=5")
+  .fromTo("#lucifer", 
+  { opacity: '0%', scaleX: '0' , scaleY:'0'}, 
+  { opacity: '100%', scaleX: '100%', scaleY:'100%', duration:2 },"+=5")
+  .fromTo("#luciferDance", 
+  { skewX:"20%", scaleX: "100%"}, 
+  { skewX:"-20%", scaleX: "-100%", repeat: "-1", yoyo:"true", ease:"none"},"+=5")
+  .fromTo("#luciferVol", 
+  { scaleY:"100%"}, 
+  { scaleY:"95%", duration:"0.05", repeat: "-1", yoyo:"true", ease:"none",transformOrigin:"right 100%"},"+=5")
+  .fromTo("#main", 
+  { y:"-100%", opacity:"0%"}, 
+  { y:"0", opacity:"100%"},"+=5")
+  .fromTo("#luciferChute", 
+  { y:"5%"}, 
+  { y:"0", yoyo:"true", repeat:"-1", ease:"none"},"+=5")
+  .fromTo("#luciferTriste", 
+  { rotate:5}, 
+  { rotate:"-5", yoyo:"true", repeat:"-1", ease:"none"},"+=5")
+  .fromTo("#luciferTriste", 
+  { x:"10%"}, 
+  { x:"-50%", duration: "3"},"+=5")
